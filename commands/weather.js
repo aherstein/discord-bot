21d71334837e9a1117c96589b3835828840b4a97
Adam Herstein <7376801+aherstein@users.noreply.github.com>
2019-06-14 12:23:53 -0700

    Moved geolocation to separate function, additional logging

--- commands/weather.js
+++ commands/weather.js
@@ -1,14 +1,22 @@
 const credentials = require('../credentials')
 const moment = require('moment')
 const axios = require('axios')
-const debug = require('debug')('bot')
+const debug = require('debug')('bot:weather')
 
 module.exports = {
   darkSkyBaseUri: 'https://api.darksky.net/forecast/' + credentials.darksky + '/',
   bingMapsBaseUri: 'http://dev.virtualearth.net/REST/v1/Locations?key=' + credentials.bingmaps + '&',
 
-  isItRaining: function (location) {
-
+  /**
+   *
+   * @param location string
+   * @returns {Promise<Object>} {
+          lat: lat,
+          long: long,
+          name: name
+        }
+   */
+  geoCode: function (location) {
     return new Promise((resolve, reject) => {
       // Split city from state
       let address = location.split(' ')
@@ -27,24 +35,43 @@ module.exports = {
 
       axios.get(this.bingMapsBaseUri + params).then(response => {
         if (response.data.resourceSets[0].resources.length === 0) {
-          resolve('Sorry, I don\'t understand the location ' + location + '!')
+          reject()
+        } else {
+          let geoData = response.data.resourceSets[0].resources[0]
+          let lat = geoData.point.coordinates[0]
+          let long = geoData.point.coordinates[1]
+          let name = geoData.name
+
+          let results = {
+            lat: lat,
+            long: long,
+            name: name
+          }
+
+          resolve(results)
         }
+      }).catch(err => {
+        debug(err)
+        reject()
+      })
+    })
+  },
 
-        let geoData = response.data.resourceSets[0].resources[0]
-        let lat = geoData.point.coordinates[0]
-        let long = geoData.point.coordinates[1]
-
-        axios.get(this.darkSkyBaseUri + lat + ',' + long).then(response => {
+  isItRaining: function (location) {
+    return new Promise((resolve, reject) => {
+      this.geoCode(location).then(geo => {
+        axios.get(this.darkSkyBaseUri + geo.lat + ',' + geo.long).then(response => {
           if (response.data.currently.icon === 'rain') {
-            resolve('It is raining in ' + geoData.name)
+            resolve('It is raining in ' + geo.name)
           } else {
-            resolve('It is NOT raining in ' + geoData.name)
+            resolve('It is NOT raining in ' + geo.name)
           }
         }).catch(err => {
           debug(err)
+          resolve('Sorry, I don\'t understand the location ' + location + '!')
         })
-      }).catch(err => {
-        debug(err)
+      }).catch(() => {
+        resolve('Sorry, I don\'t understand the location ' + location + '!')
       })
     })
   }
