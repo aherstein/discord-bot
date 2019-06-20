8beec1171baadad7d40e31c4513326aed30541d8
Adam Herstein <7376801+aherstein@users.noreply.github.com>
2019-06-20 11:37:53 -0700

    Created command class and allowed environment-specific configuration of command character

--- index.js
+++ index.js
@@ -8,49 +8,14 @@ const winston = require('winston')
 const utils = require('./util')
 const Postgres = require('pg')
 const pg = new Postgres.Client()
+const Command = require('./command')
 
 // Commands
 const commandsTime = require('./commands/time')
 const commandsWeather = require('./commands/weather')
 const commandsPokedex = require('./commands/pokedex')
 
-const commandChar = '/'
 const darkSkyAttribution = new Discord.RichEmbed().setTitle('Powered by Dark Sky').setURL('https://darksky.net/poweredby/')
-
-/**
- * Strips command char and any mentions
- *
- * @param msg
- * @returns {Promise<string>}
- */
-async function formatMessage (msg) {
-  let re = new RegExp('\\' + commandChar, 'g')
-  return msg.content.replace(re, '') // Remove command char
-    .replace(/^<.*> /g, '') // Remove all mentions
-    .toLowerCase()
-}
-
-/**
- * Returns command
- *
- * @param msg
- * @returns {Promise<string>}
- */
-async function parseCommand (msg) {
-  const actualMessage = await formatMessage(msg)
-  return actualMessage.split(' ')[0]
-}
-
-/**
- * Returns array of command parameters
- *
- * @param msg
- * @returns {Promise<string[]>}
- */
-async function parseParameters (msg) {
-  const actualMessage = await formatMessage(msg)
-  return actualMessage.split(' ').splice(1) // Remove first param as it's the command and return the rest as parameters
-}
 
 discord.on('ready', () => {
   debug('Logged in as %s!', discord.user.tag)
@@ -59,66 +24,59 @@ discord.on('ready', () => {
 discord.on('message', msg => {
   let startTime = moment()
 
-  if (msg.content.startsWith(commandChar)) {
-    formatMessage(msg).then(actualMessage => {
-      debug('%s @%s #%s %s', msg.guild.name, msg.member.displayName, msg.channel.name, actualMessage)
+  const command = new Command(msg)
 
-      parseCommand(msg).then((command) => {
+  if (command.isValid()) {
+    debug('%s @%s #%s %s', msg.guild.name, msg.member.displayName, msg.channel.name, command.actualMessage)
 
-        /** time */
-        if (command === 'time') {
-          msg.channel.send(commandsTime.currentTime())
-        }
+    /** time */
+    if (command.command === 'time') {
+      msg.channel.send(commandsTime.currentTime())
+    }
 
-        /** weather */
-        if (command === 'weather') {
-          parseParameters(msg).then((params) => {
-            if (params[0] === 'raining') {
-              commandsWeather.isItRaining(utils.getStringifiedParams(params)).then((message) => {
-                msg.channel.send(message, darkSkyAttribution)
-              })
-            }
-            if (params[0] === 'forecast') {
-              commandsWeather.forecast(utils.getStringifiedParams(params)).then((message) => {
-                msg.channel.send(message, darkSkyAttribution)
-              })
-            }
+    /** weather */
+    if (command.command === 'weather') {
+      if (command.params[0] === 'raining') {
+        commandsWeather.isItRaining(utils.getStringifiedParams(command.params)).then((message) => {
+          msg.channel.send(message, darkSkyAttribution)
+        })
+      }
+      if (command.params[0] === 'forecast') {
+        commandsWeather.forecast(utils.getStringifiedParams(command.params)).then((message) => {
+          msg.channel.send(message, darkSkyAttribution)
+        })
+      }
+    }
+
+    /** pokedex */
+    if (command.command === 'pokedex' || command.command === 'pokédex') {
+      if (command.params[0] === 'sprite') {
+        commandsPokedex.sprite(utils.getStringifiedParams(command.params)).then((spriteUrl) => {
+          msg.channel.send(new Discord.Attachment(spriteUrl))
+        }).catch(() => {
+          msg.channel.send('Sorry, I\'ve never heard of that Pokémon!')
+        })
+      } else { // info
+        let pokemon = utils.getStringifiedParams(command.params, true)
+        commandsPokedex.info(pokemon).then((info) => {
+          commandsPokedex.sprite(pokemon).then((spriteUrl) => {
+            let sprite = new Discord.Attachment(spriteUrl)
+            msg.channel.send(info, sprite)
+          }).catch(() => {
+            msg.channel.send('Sorry, I\'ve never heard of that Pokémon!')
           })
-        }
+        }).catch(() => {
+          msg.channel.send('Sorry, I\'ve never heard of that Pokémon!')
+        })
+      }
+    }
 
-        /** pokedex */
-        if (command === 'pokedex' || command === 'pokédex') {
-          parseParameters(msg).then((params) => {
-            if (params[0] === 'sprite') {
-              commandsPokedex.sprite(utils.getStringifiedParams(params)).then((spriteUrl) => {
-                msg.channel.send(new Discord.Attachment(spriteUrl))
-              }).catch(() => {
-                msg.channel.send('Sorry, I\'ve never heard of that Pokémon!')
-              })
-            } else { // info
-              let pokemon = utils.getStringifiedParams(params, true)
-              commandsPokedex.info(pokemon).then((info) => {
-                commandsPokedex.sprite(pokemon).then((spriteUrl) => {
-                  let sprite = new Discord.Attachment(spriteUrl)
-                  msg.channel.send(info, sprite)
-                }).catch(() => {
-                  msg.channel.send('Sorry, I\'ve never heard of that Pokémon!')
-                })
-              }).catch(() => {
-                msg.channel.send('Sorry, I\'ve never heard of that Pokémon!')
-              })
-            }
-          })
-        }
-
-        /** ping */
-        if (command === 'ping') {
-          msg.channel.send(moment().diff(startTime) + ' ms')
-        }
-      })
-    })
-
+    /** ping */
+    if (command.command === 'ping') {
+      msg.channel.send(moment().diff(startTime) + ' ms')
+    }
   }
+
 })
 
 discord.login(credentials.bot).catch((err) => {
