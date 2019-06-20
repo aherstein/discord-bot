2be9a80f352a44b735a9f11188edc698f9ce1fd2
Adam Herstein <7376801+aherstein@users.noreply.github.com>
2019-06-20 15:15:19 -0700

    Changed bot to use commando framework

--- commands/time.js
+++
@@ -1,7 +0,0 @@
-const moment = require('moment')
-
-module.exports = {
-  currentTime: function () {
-    return moment().format('YYYY-MM-DD HH:mm:SS Z')
-  }
-}
---
+++ commands/time/time.js
@@ -0,0 +1,23 @@
+const debug = require('debug')('bot:pokedex')
+const moment = require('moment')
+const commando = require('discord.js-commando')
+const oneLine = require('common-tags').oneLine
+
+module.exports = class Time extends commando.Command {
+  constructor (client) {
+    super(client, {
+      name: 'time',
+      aliases: [],
+      group: 'time',
+      memberName: 'time',
+      description: 'Get current time.',
+      details: oneLine`
+      `,
+      examples: ['time'],
+    })
+  }
+
+  async run (msg, args) {
+    return msg.channel.send(moment().format('YYYY-MM-DD HH:mm:SS Z'))
+  }
+}
